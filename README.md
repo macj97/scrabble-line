da5e667ebe618bacf4003fb1766e62c861930885
Joe Plummer <jmplumm97@gmail.com>
2026-06-20 00:39:48 -0400

    Have basic drag and drop functionality set, but only displays a pop up when an object is dragged to a space, also tried implenting styling

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # scrabble-line
 Implementing a bit of Scrabble with drag-and-drop for Assignment 5 of the class GUI I at UML.
+
+Scrabble bag image: https://i.etsystatic.com/9912930/r/il/fc9e74/4329904185/il_600x600.4329904185_403m.jpg
+
+
--- js/script.js
+++ js/script.js
@@ -5,3 +5,64 @@
  * Joe Plummer, UMass Lowell Computer Science, joseph_plummer@student.uml.edu
  * Copyright (c) 2026 by Joe Plummer. All rights reserved. May be freely copied or excerpted for educational purposes with credit to the author.
  */
+
+$(function() { //jQuery ready function
+
+    $("#draggable").draggable({ revert: "invalid" });
+    $("#draggable1").draggable({ revert: "invalid" });
+    $("#draggable2").draggable({ revert: "invalid" });
+    $("#draggable3").draggable({ revert: "invalid" });
+    $("#draggable4").draggable({ revert: "invalid" });
+    $("#draggable5").draggable({ revert: "invalid" });
+    $("#draggable6").draggable({ revert: "invalid" });
+
+    $("#droppable").droppable({
+        drop: function() {
+            console.log("item has been dropped");
+            alert("item has been dropped");
+            // $(this).addClass("ui-state-highlight")
+            //         .find("p")
+            //         .html("Dropped!");
+                    
+        }
+    });
+    $("#droppable1").droppable({
+        drop: function() {
+            console.log("item has been dropped");
+            alert("item has been dropped");
+        }
+    });
+    $("#droppable2").droppable({
+        drop: function() {
+            console.log("item has been dropped");
+            alert("item has been dropped");
+        }
+    });
+    $("#droppable3").droppable({
+        drop: function() {
+            console.log("item has been dropped");
+            alert("item has been dropped");
+        }
+    });
+    $("#droppable4").droppable({
+        drop: function() {
+            console.log("item has been dropped");
+            alert("item has been dropped");
+        }
+    });
+    $("#droppable5").droppable({
+        drop: function() {
+            console.log("item has been dropped");
+            alert("item has been dropped");
+        }
+    });
+    $("#droppable6").droppable({
+        drop: function() {
+            console.log("item has been dropped");
+            alert("item has been dropped");
+        }
+    });
+
+
+
+});
